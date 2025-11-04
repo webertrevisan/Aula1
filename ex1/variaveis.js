66717b9b64f11b0e2b55877c3a98586823d82c3d
Weber Trevisan <weber.trevisan@gmail.com>
2025-11-04 20:59:59 -0300

    Arrows

--- ex1/variaveis.js
+++ ex1/variaveis.js
@@ -4,9 +4,9 @@ const nome = 'weber';
 let idade = 31;
 var cidade = 'Ribeirao Preto';
 
-// console.log(nome, idade, cidade);
+console.log(nome, idade, cidade);
 
-// console.log(`Meu nome é ${nome} moro na ciadade ${cidade} e tenho ${idade} anos`)
+console.log(`Meu nome é ${nome} moro na ciadade ${cidade} e tenho ${idade} anos`)
 
 // --Funções
 
@@ -14,10 +14,15 @@ function soma(a, b) { // função
     return a + b;    
 }
 
-const somaArrow = (a, b) => {
-    return a + b
-};
+// const somaArrow = (a, b) => {
+//     return a + b
+// };
 
+const somaArrow = (a, b) => a + b ;
+const mutiplica = x => x * 2 ;
+const divide = () => 10/2;
+
+console.log(divide());
 console.log(`Soma tradicional: ${soma(2, 3)}`);
-
 console.log(`Soma arrow function: ${somaArrow(2, 3)}`);
+console.log(`multiplica por 2: ${mutiplica(5)}`)
